df03fbf52557b4eb2e51a48a9b8386fdb3ee47dd
JObit <lagosjavier223@gmail.com>
2023-01-30 19:39:06 +0100

    Emboar

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -19238,4 +19238,18 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		eggGroups: ["Undiscovered"],
 		gen: 5,
 	},
+	emboarjavi: {
+		num: 10000,
+		name: "Emboar",
+		types: ["Water", "Fire"],
+		genderRatio: {M: 0.875, F: 0.125},
+		baseStats: {hp: 110, atk: 123, def: 80, spa: 70, spd: 80, spe: 65},
+		abilities: {0: "Overgrow", 1: "Magic Guard", H: "Intrepid Sword"},
+		heightm: 1.6,
+		weightkg: 150,
+		color: "Red",
+		prevo: "Pignite",
+		evoLevel: 36,
+		eggGroups: ["Field"],
+	},
 };
