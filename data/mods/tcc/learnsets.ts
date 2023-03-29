1013464e66ac39b81c9083b5dd94b7e3d639bace
JObit <lagosjavier223@gmail.com>
2023-03-29 20:37:38 +0200

    TCC mod.

---
+++ data/mods/tcc/learnsets.ts
@@ -0,0 +1,10 @@
+export const Learnsets: {[k: string]: ModdedLearnsetData} = {
+	pigardoom: {
+		learnset: {
+			surgingstrikes: ["8L0"],
+			suckerpunch: ["8L0"],
+			fishiousrend: ["8L0"],
+			flareblitz: ["8L0"],
+		},
+	}
+};
