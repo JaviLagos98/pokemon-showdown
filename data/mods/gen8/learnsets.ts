0edccfc7b380e9fd113ee3f0b1c292246ac4b072
JObit <lagosjavier223@gmail.com>
2023-03-29 23:16:07 +0200

    Emboar Unban.

--- data/mods/gen8/learnsets.ts
+++ data/mods/gen8/learnsets.ts
@@ -3,4 +3,12 @@ export const Learnsets: {[k: string]: ModdedLearnsetData} = {
 		inherit: true,
 		eventOnly: true,
 	},
+	pigardoom: {
+		learnset: {
+			surgingstrikes: ["8L0"],
+			suckerpunch: ["8L0"],
+			fishiousrend: ["8L0"],
+			flareblitz: ["8L0"],
+		},
+	},
 };
