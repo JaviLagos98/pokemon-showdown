c2e4ba40f54a5030768c7ec09c596d5b99043380
JObit <lagosjavier223@gmail.com>
2023-05-15 22:56:22 +0200

    Formats.

---
+++ config/custom-formats.ts
@@ -0,0 +1,31 @@
+export const Formats: FormatList = [
+
+    // TECA TWICH CUP
+	///////////////////////////////////////////////////////////////////
+
+    {
+		section: "TECA TWICH CUP",
+		column: 1,
+	},
+	{
+		name: "[Gen 8] [TCC] TECA TWICH CUP 2 Singles",
+
+		mod: 'gen8',
+		ruleset: [
+            'Obtainable Moves', 'Obtainable Abilities', 'Team Preview', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Endless Battle Clause', 'Item Clause',
+            'OHKO Clause', 'Evasion Items Clause', 'Evasion Moves Clause', 'Max Team Size = 8', 'Default Level = 100',
+            'Picked Team Size = 6', 'Dynamax Clause',
+        ],
+	},
+    {
+		name: "[Gen 8] [TCC] TECA TWICH CUP 2 Doubles",
+
+		mod: 'gen8',
+        gameType: 'doubles',
+		ruleset: [
+			'Obtainable Moves', 'Obtainable Abilities', 'Team Preview', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Item Clause', 'Endless Battle Clause', 
+            'OHKO Clause', 'Evasion Items Clause', 'Evasion Moves Clause', 'Max Team Size = 8', 'Default Level = 100',
+            'Picked Team Size = 4', 'Dynamax Clause',
+        ],
+	},
+];
